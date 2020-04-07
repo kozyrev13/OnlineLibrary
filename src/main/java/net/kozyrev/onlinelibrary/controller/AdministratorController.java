1aac52356db50b22003744638f16124d55da63ba
Max <kozyrevjob@gmail.com>
2020-04-07 23:20:27 +0300

    refactoring

--- src/main/java/net/kozyrev/onlinelibrary/controller/AdministratorController.java
+++ src/main/java/net/kozyrev/onlinelibrary/controller/AdministratorController.java
@@ -1,17 +1,13 @@
 package net.kozyrev.onlinelibrary.controller;
 
 import net.kozyrev.onlinelibrary.model.Book;
-import net.kozyrev.onlinelibrary.model.IssuedBook;
-import net.kozyrev.onlinelibrary.model.LibraryUser;
 import net.kozyrev.onlinelibrary.service.BookService;
 import net.kozyrev.onlinelibrary.service.IssuedBookService;
 import net.kozyrev.onlinelibrary.service.LibraryUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AdministratorController {
 
     @Autowired
-    private BookService bookService;
+    private BookService defaultBookService;
 
     @Autowired
     private IssuedBookService issuedBookService;
@@ -27,21 +23,18 @@ public class AdministratorController {
     @Autowired
     private LibraryUserService libraryUserService;
 
-
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String adminLists(Model model) {
         model.addAttribute("book", new Book());
-        model.addAttribute("bookList",bookService.listBook());
+        model.addAttribute("bookList", defaultBookService.listBook());
         model.addAttribute("issuedBookList",issuedBookService.listIssuedBook());
         model.addAttribute("libraryUserList",libraryUserService.getList());
-
         return "admin";
     }
 
     @RequestMapping(value = "/admin", method = RequestMethod.POST)
     public String addBook(@ModelAttribute("book") Book book) {
-            bookService.addBook(book);
-
+        defaultBookService.addBook(book);
         return "redirect:admin";
     }
 
